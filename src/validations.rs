c12d3b873d81b63457f6fc0cf8319be840f3b77a
HosMercury <hos.mercury@gmail.com>
2024-03-16 22:01:51 +0200

    remove drafts

--- src/validations.rs
+++ src/validations.rs
@@ -10,32 +10,6 @@ lazy_static! {
     pub static ref REGEX_USERNAME: Regex = Regex::new(r"[a-zA-Z0-9_-]{8,50}$").unwrap();
 }
 
-pub fn flatten_validation_errs<'a>(
-    e: &'a ValidationErrors,
-    new_errors: &'a mut ValidationErrors,
-) -> &'a ValidationErrors {
-    e.errors().into_iter().for_each(|(field, kind)| {
-        // println!("{}", field);
-        match kind {
-            ValidationErrorsKind::Struct(errors) => {
-                flatten_validation_errs(&*errors, new_errors);
-            }
-            ValidationErrorsKind::List(errors_list) => {
-                errors_list.clone().into_iter().for_each(|(_, errors)| {
-                    flatten_validation_errs(&*errors, new_errors);
-                });
-            }
-            ValidationErrorsKind::Field(errors) => {
-                errors.into_iter().enumerate().for_each(|(_, error)| {
-                    new_errors.add(field, error.clone());
-                });
-            }
-        }
-    });
-
-    new_errors
-}
-
 // add Localized messages to validation errors
 pub fn validation_messages(errors: &ValidationErrors) -> ValidationErrors {
     let mut locale_errors: ValidationErrors = ValidationErrors::new();
@@ -169,6 +143,33 @@ pub fn validation_messages(errors: &ValidationErrors) -> ValidationErrors {
     locale_errors
 }
 
+#[allow(unused)]
+pub fn flatten_validation_errs<'a>(
+    e: &'a ValidationErrors,
+    new_errors: &'a mut ValidationErrors,
+) -> &'a ValidationErrors {
+    e.errors().into_iter().for_each(|(field, kind)| {
+        // println!("{}", field);
+        match kind {
+            ValidationErrorsKind::Struct(errors) => {
+                flatten_validation_errs(&*errors, new_errors);
+            }
+            ValidationErrorsKind::List(errors_list) => {
+                errors_list.clone().into_iter().for_each(|(_, errors)| {
+                    flatten_validation_errs(&*errors, new_errors);
+                });
+            }
+            ValidationErrorsKind::Field(errors) => {
+                errors.into_iter().enumerate().for_each(|(_, error)| {
+                    new_errors.add(field, error.clone());
+                });
+            }
+        }
+    });
+
+    new_errors
+}
+
 ////////////////////////////////// Validation fns //////////////////////////////
 // No async needing so return Result to automatically merge with Validator
 pub fn validate_password(password: &str) -> Result<(), ValidationError> {
@@ -222,70 +223,4 @@ pub async fn email_exists(email: &str, pool: &Pool<Postgres>) -> bool {
         .fetch_one(pool)
         .await
         .is_ok()
-}
-
-///////////////////////////////////////////////////////////////////////
-////////////////////////////////// Drafts /////////////////////////////
-
-#[allow(unused)]
-pub fn flash_errors(errs: ValidationErrors, messages: Messages) {
-    let mut new_errs = ValidationErrors::new();
-
-    flatten_validation_errs(&errs, &mut new_errs)
-        .field_errors()
-        .into_iter()
-        .for_each(|(field, errs)| {
-            errs.into_iter().for_each(|e| {
-                let ms = messages.clone();
-                // println!("{:#?} - {}", field, e.message.clone().unwrap());
-                ms.error(format!("{} - {}", field, e.message.clone().unwrap()));
-            })
-        });
-
-    // println!("mmm {:?}", messages);
-}
-
-#[allow(unused)]
-pub fn extract_errors(
-    errors: HashMap<&'static str, ValidationErrorsKind>,
-) -> HashMap<String, String> {
-    let mut extracted_errs: HashMap<String, String> = HashMap::new();
-    for (k, v) in errors {
-        match v {
-            ValidationErrorsKind::Struct(_) => {} // todo
-            ValidationErrorsKind::List(_) => {}   // todo
-            ValidationErrorsKind::Field(errs) => {
-                for err in errs {
-                    let msg = err.message.as_ref().unwrap();
-                    extracted_errs.insert(k.to_string(), msg.to_string());
-                }
-            }
-        }
-    }
-    extracted_errs
-}
-
-#[allow(unused)]
-pub fn pretty_print(e: &ValidationErrors, depth: usize) {
-    match format_args!("{:1$}", "", depth * 2) {
-        indent => {
-            e.errors()
-                .iter()
-                .for_each(|(field_name, error_kind)| match error_kind {
-                    ValidationErrorsKind::Field(error_messages) => {
-                        error_messages
-                            .iter()
-                            .for_each(|m| println!("{indent}  {m},"));
-                    }
-                    ValidationErrorsKind::Struct(nested) => {
-                        pretty_print(nested, depth + 1);
-                    }
-                    ValidationErrorsKind::List(sub_array) => {
-                        sub_array.iter().for_each(|(i, nested)| {
-                            pretty_print(nested, depth + 2);
-                        });
-                    }
-                });
-        }
-    }
 }
